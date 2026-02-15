18f2ca47f31d22c7cc769bd54557897dfb1cf63c
Nova TI Devs <70083106+ItsCrokYT@users.noreply.github.com>
2026-02-15 15:00:37 -0600

    Actualizacion

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/nombre-de-tu-repositorio/", // <--- EJEMPLO: "/nova-web-agency/"
+  base: "/NOVA-Web-Agency", // <--- EJEMPLO: "/nova-web-agency/"
 })
